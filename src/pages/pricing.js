b1eeca260c3c78d630667af6afe8bfa7b2fa3736
Calvin Smith <smithcalvin33@gmail.com>
2024-04-27 15:35:16 -0400

    feat: personalise site for Meaghan (#1)
    * feat: personalise site for Meaghan
    
    * feat: added pricing page and images
    
    * feat: bigger favicon
    
    * feat: fix images

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -4,9 +4,9 @@ import { LegalSection, Page, Seo } from "gatsby-theme-portfolio-minimal";
 export default function ImprintPage() {
   return (
     <>
-      <Seo title="Imprint" useTitleTemplate={true} noIndex={true} />
+      <Seo title="Pricing" useTitleTemplate={true} noIndex={true} />
       <Page>
-        <LegalSection sectionId="imprint" heading="Imprint" />
+        <LegalSection sectionId="pricing" heading="Pricing" />
       </Page>
     </>
   );
